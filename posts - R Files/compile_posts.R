ad4c6261a0152e56473b5c7af2f3074de6978801
andeek <ajkaplan@iastate.edu>
2014-02-01 20:32:36 -0600

    forgot jquery

--- posts - R Files/compile_posts.R
+++ posts - R Files/compile_posts.R
@@ -7,4 +7,4 @@ KnitPost <- function(input, base.url = "/") {
   render_jekyll()
   knit(input, envir = parent.frame())
 }
-KnitPost("2014-02-010reproducible-research.Rmd", '../../../../')
+KnitPost("2014-02-01-reproducible-research.Rmd", '../../../../')
