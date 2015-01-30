553805eea1e9f69529b81d43849a6280b3a194df
Andee Kaplan <ajkaplan@iastate.edu>
2015-01-30 09:37:16 -0600

    trying wegbl

--- posts - R Files/compile_posts.R
+++ posts - R Files/compile_posts.R
@@ -7,4 +7,4 @@ KnitPost <- function(input, base.url = "/") {
   render_jekyll()
   knit(input, envir = parent.frame())
 }
-KnitPost("2015-01-19-DataStories.Rmd", '../../../../')
+KnitPost("2015-01-29-ModelVis.Rmd", '../../../../')
